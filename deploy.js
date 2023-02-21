0db3081e6c14c5300a9d58df28fe91675feda417
Riad Elimemmedov <79299168+riadelimemmedov@users.noreply.github.com>
2023-02-21 16:08:16 +0400

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -18,7 +18,7 @@ const web3 = new Web3(provider);
 const deploy = async () => {
     const accounts = await web3.eth.getAccounts();
 
-    console.log('Attempting to deploy from account', accounts[0]);
+    console.log('Attempting to deploy from account...', accounts[0]);
 
     const result = await new web3.eth.Contract(abi)//Create and Deploy contratc,and send Transaction
         .deploy({
